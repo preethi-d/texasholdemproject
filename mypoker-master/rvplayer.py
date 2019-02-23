ac8e71829fb756beda0ffcd2205f53ba5460a5e1
“jamessspanggg” <jamespangmunwai@gmail.com>
2019-02-23 23:05:23 +0800

    Add new player

---
+++ mypoker-master/rvplayer.py
@@ -0,0 +1,25 @@
+from pypokerengine.players import BasePokerPlayer
+
+class RVPlayer(BasePokerPlayer):
+    def declare_action(self, valid_actions, hole_card, round_state):
+        # add code here
+        pass
+
+    def receive_game_start_message(self, game_info):
+        pass
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+
+
+def setup_ai():
+    return RVPlayer()
