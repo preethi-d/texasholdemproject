144589ab564768a5076d257422cee1c7b6aba0ab
jamessspanggg <jamespangmunwai@gmail.com>
2019-03-17 18:26:43 +0800

    Update rvplayer

--- mypoker-master/testperf.py
+++ mypoker-master/testperf.py
@@ -22,7 +22,7 @@ $ python testperf.py -n1 "Random Warrior 1" -a1 RandomPlayer -n2 "Random Warrior
 def testperf(agent_name1, agent1, agent_name2, agent2):		
 
 	# Init to play 500 games of 1000 rounds
-	num_game = 100
+	num_game = 10
 	max_round = 1000
 	initial_stack = 10000
 	smallblind_amount = 20
@@ -36,7 +36,7 @@ def testperf(agent_name1, agent1, agent_name2, agent2):
 	
 	# Register players
 	config.register_player(name=agent_name1, algorithm=RVPlayer())
-	config.register_player(name=agent_name2, algorithm=RandomPlayer())
+	config.register_player(name=agent_name2, algorithm=RVPlayer())
 	# config.register_player(name=agent_name1, algorithm=agent1())
 	# config.register_player(name=agent_name2, algorithm=agent2())
 	
